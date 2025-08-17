49cceedb7277f123c8c442c58f4de37bcb02f7ae
Biswajit <chanda.b0@gmail.com>
2025-08-17 13:47:42 +0530

    Changed meesage

--- src/main/java/com/demo/cicd/HelloController.java
+++ src/main/java/com/demo/cicd/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot CI/CD Demo!";
+        return "Hello from Spring Boot CI/CD Demo!!!";
     }
 }
