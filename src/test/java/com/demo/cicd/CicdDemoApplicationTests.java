be36458c493cd60989a7e094c77b44b445e2b3f3
Biswajit <chanda.b0@gmail.com>
2025-08-17 13:35:24 +0530

    Converted to spring boot app

---
+++ src/test/java/com/demo/cicd/CicdDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.demo.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
